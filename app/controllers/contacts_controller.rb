5b75e89179f120a30ac4e17d519e8c1506ddbee4
Josh Espinosa <jespsf@gmail.com>
2015-11-17 01:08:46 +0000

    Added mailer and view

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -10,6 +10,12 @@ class ContactsController < ApplicationController
     @contact = Contact.new(contact_params) #this code creates a new Contact object populated with form data or contact_params which is a variable defined in private section
     
     if @contact.save #so save it and if it worked or didnt work, run whatever part of if statement needs to be run
+        
+        name = params[:contact][:name] #New contact's name
+        email = params[:contact][:email] #New contact's email
+        body = params[:contact][:comments] #New contact's comments
+        ContactMailer.contact_email(name, email, body).deliver
+        
         flash[:success] = 'Message sent.'
         redirect_to new_contact_path
     else
